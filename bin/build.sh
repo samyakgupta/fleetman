b44d828f7954621ae894d098ad5e9ec3cd21e61e
SAMYAK GUPTA <samyakgupt@gmail.com>
2019-12-11 12:56:47 +0530

    Updated
    added line to bind user to docker service

--- bin/build.sh
+++ bin/build.sh
@@ -4,6 +4,7 @@ for docker in $(find ./ -name Dockerfile); do
   name=$(dirname $docker)
   echo ${name#./}
   (cd $name && docker build -t ${name#./} .)
+  sudo usermod -aG docker $USER
   $(aws ecr get-login --no-include-email --region ap-south-1)
   aws ecr create-repository --repository-name ${name#./} --image-tag-mutability IMMUTABLE
   docker tag ${name#./}:latest 580572941932.dkr.ecr.ap-south-1.amazonaws.com/${name#./}:latest
