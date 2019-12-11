b50ac7af045212f60e3e942e4e23a020cd040ef6
SAMYAK GUPTA <samyakgupt@gmail.com>
2019-12-11 12:00:03 +0530

    Updated build.sh
    made it to work with cloudenablers

--- bin/build.sh
+++ bin/build.sh
@@ -5,7 +5,6 @@ for docker in $(find ./ -name Dockerfile); do
   echo ${name#./}
   (cd $name && docker build -t ${name#./} .)
   $(aws ecr get-login --no-include-email --region ap-south-1)
-  aws ecr create-repository --repository-name ${name#./} --image-tag-mutability IMMUTABLE
-  docker tag ${name#./}:latest 580572941932.dkr.ecr.ap-south-1.amazonaws.com/${name#./}:latest
-  docker push 580572941932.dkr.ecr.ap-south-1.amazonaws.com/${name#./}:latest
+  docker tag ${name#./}:latest 580572941932.dkr.ecr.ap-south-1.amazonaws.com/cloudenablers/${name#./}:latest
+  docker push 580572941932.dkr.ecr.ap-south-1.amazonaws.com/cloudenablers/${name#./}:latest
 done
